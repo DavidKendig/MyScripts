4b1136820e2782268a306e0717c447e3c5ae0300
David Kendig <trip118.scifi@yahoo.com>
2025-12-20 13:03:13 -0500

    Updated Java PATH for JDK 25

--- SetJava.bat
+++ SetJava.bat
@@ -1,2 +1,18 @@
-setx -m JAVA_HOME "C:\Progra~1\Microsoft\jdk-16.0.1.9-hotspot
-setx -m JRE_HOME "C:\Progra~1\Java\jre1.8.0_291
+@echo off
+REM Set Java JDK 25 as the active Java version
+REM Run this script as Administrator to set system-wide variables
+
+echo Setting JAVA_HOME to JDK 25...
+setx JAVA_HOME "C:\Program Files\Java\jdk-25"
+
+echo Adding Java to PATH...
+setx PATH "%PATH%;%JAVA_HOME%\bin"
+
+echo.
+echo Java environment variables set successfully!
+echo.
+echo To verify, close this window and open a new command prompt, then run:
+echo   java -version
+echo   javac -version
+echo.
+pause
